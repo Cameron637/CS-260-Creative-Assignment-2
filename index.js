1f6ed7bb1a3a28819fc6a16e6e3d0b852f395d3b
Cameron Sanchez <csanchez2237@gmail.com>
2017-10-04 20:43:38 -0600

    get sunrise/sunset info

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-$(document).ready(function() {
-$("#submit").click(function(e){
-  console.log("#zip");
-  console.log("#city");
-  var value = $("#zip").val();
-  var latitude;
-  var longitude
-  console.log(value);
-  e.preventDefault();
-$("#displayCity").text(value);
-  var myurl= "http://api.zippopotam.us/us/";
-  myurl += value;
-  //myurl += ".json";
-  console.log(myurl);
-  $.getJSON(myurl, function(parsed_json) {
-      latitude = parsed_json['places']['0']['latitude'];
-      longitude = parsed_json['places']['0']['longitude'];
-      everything = "<ul>";
-      everything += "<li>Latitude: "+latitude;
-      everything += "<li>Longitude: "+longitude;
-      everything += "</ul>";
-      $("#coordinate").html(everything);
-  })
-  .done(function() {
-    var sunriseURL = 'https://api.sunrise-sunset.org/json?lat=' + latitude + '&lng=' + longitude + '&date=today';
-    $.getJSON(sunriseURL, function(parsed_json) {
-      console.log(parsed_json);
-    });
+'use strict';
+
+$(document).ready(function () {
+  $('#zip').on('keyup', function(key) {
+    if (key.which === 13) {
+      $('#submit').click();
+    }
   });
 
-});
+  $('#submit').on('click', function () {
+    let latitude;
+    let longitude;
+    let latLongURL = 'http://api.zippopotam.us/us/' + $('#zip').val();
+
+    $.getJSON(latLongURL, function (json) {
+      let city = json.places[0]['place name'] + ', ' + json.places[0]['state abbreviation'];
+      latitude = json.places[0].latitude;
+      longitude = json.places[0].longitude;
+      let html = '<ul>';
+      html += '<li>Latitude: ' + latitude;
+      html += '<li>Longitude: ' + longitude;
+      html += '</ul>';
+      $('#coordinate').html(html);
+      $('#suncycle h2 span').text(city);
+    })
+      .done(function () {
+        let sunriseURL = 'https://api.sunrise-sunset.org/json?lat=' + latitude + '&lng=' + longitude + '&date=today';
+
+        $.getJSON(sunriseURL, function (json) {
+          $('#sunrise h3 span').text(json.results.sunrise);
+          $('#sunset h3 span').text(json.results.sunset);
+        });
+      });
+  });
 });
