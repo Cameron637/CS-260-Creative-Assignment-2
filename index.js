b469b5b72b4324ab0a9427e8bee60ad84b966fb1
1990platypus <1990platypus@gmail.com>
2017-10-05 01:52:49 +0000

    I changed stuff

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-'use strict';
-
-$(document).ready(function () {
-  $('#submit').on('click', function () {
-    
+$(document).ready(function() {
+$("#submit").click(function(e){
+  console.log("#zip");
+  console.log("#city");
+  var value = $("#zip").val();
+  console.log(value);
+  e.preventDefault();
+$("#displayCity").text(value);
+  var myurl= "http://api.zippopotam.us/us/";
+  myurl += value;
+  //myurl += ".json";
+  console.log(myurl);
+  $.ajax({
+    url : myurl,
+    dataType : "json",
+    success : function(parsed_json) {
+      var latitude = parsed_json['places']['0']['latitude'];
+      var longitude = parsed_json['places']['0']['longitude'];
+      everything = "<ul>";
+      everything += "<li>Latitude: "+latitude;
+      everything += "<li>Longitude: "+longitude;
+      everything += "</ul>";
+      $("#coordinate").html(everything);
+    }
   });
+
+});
 });
